b9e26c483b764a5a26992dd6cf467b3c46ec9eaa
MorrisKA <kyle.morris2@nhs.net>
2024-02-25 19:13:55 +0000

    Add logic to sign in form

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,54 @@
 import './content.css'
+import {useState} from 'react'
 
 export default function Content() {
+
+    const [userInfo, setUserInfo] = useState({
+        nhsNumber: "",
+        name: "",
+        born: "",
+        age: ""
+    });
+
+    function handleChange(event) {
+        let {name, value} = event.target
+
+        if (name === "name") {
+            value = value.toUpperCase()
+        }
+
+        setUserInfo(prevUserInfo => {
+            return({...prevUserInfo, [name]:value})
+        })
+    }
+
+    async function submit() {
+        try {
+            let response = await fetch(`https://al-tech-test-apim.azure-api.net/tech-test/t2/patients/${userInfo.nhsNumber}`, {
+                headers: {
+                    "Ocp-Apim-Subscription-Key": process.env.REACT_APP_Api_Subscription_Key
+                } 
+            })
+            if(!response.ok) throw Error("BadRequest")
+            response = await response.json()
+            response = {...response, "name":response.name.split(",")[0]};
+            response = {...response, "born":(response.born.substring(6,10) + "-"  + response.born.substring(3,5) + "-" + response.born.substring(0,2))}
+
+            const detailsMatch = await response.nhsNumber === userInfo.nhsNumber && response.name === userInfo.name && response.born === userInfo.born;
+            if (!detailsMatch) throw Error("IncorrectDetails")
+            
+            const dateNow = new Date();
+            const dob = new Date(response.born);
+            const diffTime = Math.abs(dateNow - dob);
+            const age = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365.25));
+            if (age < 16) throw Error("Ineligible")
+            console.log("Details correct. Thanks for accessing the T2 Lifestyle Checker")
+        } catch (error) {
+            if (error.message === "Ineligible") console.log("You are not eligble for this service")
+            else console.log("Your details could not be found")
+        }
+    }
+
     return(
         <div className="container">
             <div className="intro-container">
@@ -10,19 +58,19 @@ export default function Content() {
             <div className = "input-container">
                 <div className="entries">
                     <div>
-                        <label for="nhsNumber">NHS Number</label>
-                        <input className="input" type="text" name="nhsNumber"></input>
+                        <label>NHS Number</label>
+                        <input className="input" type="text" name="nhsNumber" onChange={handleChange}></input>
                     </div>
                     <div>
-                        <label for="surname">Surname</label>
-                        <input className="input" type="text" name="surname"></input>
+                        <label>Surname</label>
+                        <input className="input" type="text" name="name" onChange={handleChange}></input>
                     </div>
                     <div>
-                        <label for="dateOfBirth">Date of birth</label>
-                        <input className="input" type="date" name="dateOfBirth"></input>
+                        <label>Date of birth</label>
+                        <input className="input" type="date" name="born" onChange={handleChange}></input>
                     </div>
                 </div>
-                <button className="submit">Submit</button>
+                <button className="submit" onClick={submit}>Submit</button>
             </div>
         </div>
     );
