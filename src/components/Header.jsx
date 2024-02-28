f2376faa19eb1fba1b2b03e8b574551dc2e1a811
MorrisKA <kyle.morris2@nhs.net>
2024-02-28 22:45:50 +0000

    Clicking logo / title returns to original page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,13 @@ import './header.css'
 import healthIcon from "../assets/health_icon.png"
 
 export default function Header() {
+
+    const refreshPage = () => window.location.reload()
+
     return(
         <div className="header">
-            <img src={healthIcon} className="logo" alt="Health icon" />
-            <h1 className="title">T2 Lifestyle Checker</h1>
+            <img src={healthIcon} className="logo" alt="Health icon" onClick={refreshPage}/>
+            <h1 className="title" onClick={refreshPage}>T2 Lifestyle Checker</h1>
             <h2 className="subtitle">A lifestyle checking web app</h2>
         </div>
     );
